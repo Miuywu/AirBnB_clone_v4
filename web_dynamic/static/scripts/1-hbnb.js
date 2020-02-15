6798c27863a988f3e7aa15e5568569d38d6c9f4b
Ben Cook <808@holbertonschool.com>
2020-02-15 13:10:32 -0800

    wip amenities dictionary

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+  const dict = {};
   $('input:checkbox').change(function(el){
-    console.log(el.target.dataset.name);
-    console.log(el.target.dataset);
+    if (this.checked) {
+      if (!dict[el.target.dataset.name]) {
+        dict[el.target.dataset.name] = true;
+      }
+    } else {
+      delete dict[el.target.dataset.name];
+    }
+    // $('.amenities h4').html(dict.keys());
   });
 });
