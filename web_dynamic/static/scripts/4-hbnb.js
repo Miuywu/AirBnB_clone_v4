b78797d05789ab29c20cbe2cbf37e5df37525eb2
Yashey Mateen <yasheymateen@gmail.com>
2020-02-17 19:16:33 -0800

    modified 4-hbnb file to fix formatting and indentation

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -78,38 +78,40 @@ $(document).ready(function () {
       },
       dataType: 'json',
       success: function (data) {
-	$('article').remove();
-	for (const place of result) {
-	  $(".places").append(`
-	<ARTICLE>
-	<div name="title">
-          <h2>${place.name}</h2>
-          <div class="price_by_night">${place.price_by_night}</div>
-        </div>
-        <div class="information">
-          <div class="max_guest">
-            <i class="fa fa-users fa-3x" aria-hidden="true"></i>
-            <br />
-            ${ place.max_guest } Guests
-          </div>
-          <div class="number_rooms">
-            <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
-            <br />
-            ${ place.number_rooms } Bedrooms
-          </div>
-          <div class="number_bathrooms">
-            <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
-            <br />
-            ${ place.number_bathrooms } Bathroom
-          </div>
-        </div>
-        <div class="user">
-        </div>
-        <div class="description">
-          ${ place.description }
-        </div>
-	</ARTICLE>`);
+	      $('article').remove();
+	      for (const place of result) {
+	      $(".places").append(`
+	        <ARTICLE>
+	          <div name="title">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">
+                <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+                <br />
+                ${ place.max_guest } Guests
+              </div>
+              <div class="number_rooms">
+                <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+                <br />
+                ${ place.number_rooms } Bedrooms
+              </div>
+              <div class="number_bathrooms">
+                <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+                <br />
+                ${ place.number_bathrooms } Bathroom
+              </div>
+            </div>
+            <div class="user">
+            </div>
+            <div class="description">
+              ${ place.description }
+            </div>
+	        </ARTICLE>`);
       console.log(place);
-	}
+        }
       }
     });
+  });
+});
